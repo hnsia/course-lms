03ef8b78020cbe774cad94a0dbd139371cf7d3a4
hnsia <44498178+hnsia@users.noreply.github.com>
2025-07-26 14:50:05 +0800

    feat: setup admin page

---
+++ src/app/admin/courses/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { PageHeader } from "@/components/PageHeader";
+import Link from "next/link";
+
+export default function CoursesPage() {
+  return (
+    <div className="container my-6">
+      <PageHeader title="Courses">
+        <Button asChild>
+          <Link href="/admin/courses/new">New Course</Link>
+        </Button>
+      </PageHeader>
+
+      <div>content</div>
+    </div>
+  );
+}
