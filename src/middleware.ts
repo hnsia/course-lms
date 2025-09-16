366b50f6abbd63f8a3b620073dbbf3f269a8a93b
hnsia <44498178+hnsia@users.noreply.github.com>
2025-09-16 21:26:37 +0800

    feat: setup middleware protection and arcjet protection

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,55 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { forbidden, notFound } from "next/navigation";
+import arcjet, { detectBot, shield, slidingWindow } from "@arcjet/next";
+import { env } from "./data/env/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api(.*)",
+  "/courses/:courseId/lessons/:lessonId",
+  "/products(.*)",
+]);
+
+const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
+
+const aj = arcjet({
+  key: env.ARCJET_KEY,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "CATEGORY:MONITOR", "CATEGORY:PREVIEW"],
+    }),
+    slidingWindow({
+      mode: "LIVE",
+      interval: "1m",
+      max: 100,
+    }),
+  ],
+});
+
+export default clerkMiddleware(async (auth, req) => {
+  const decision = await aj.protect(req);
+
+  if (decision.isDenied()) return forbidden();
+
+  if (isAdminRoute(req)) {
+    const user = await auth.protect();
+    if (user.sessionClaims.role !== "admin") return notFound();
+  }
+
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
