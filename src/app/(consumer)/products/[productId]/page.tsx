6f4cde6e952b3391f1d78402c43f0846fdb0f586
hnsia <44498178+hnsia@users.noreply.github.com>
2025-09-23 21:54:33 +0800

    feat: add product info page

---
+++ src/app/(consumer)/products/[productId]/page.tsx
@@ -0,0 +1,131 @@
+import { db } from "@/drizzle/db";
+import {
+  CourseSectionTable,
+  LessonTable,
+  ProductTable,
+} from "@/drizzle/schema";
+import { getCourseIdTag } from "@/features/courses/db/cache/courses";
+import { getCourseSectionCourseTag } from "@/features/courseSections/db/cache";
+import { wherePublicCourseSections } from "@/features/courseSections/permissions/sections";
+import { getLessonCourseTag } from "@/features/lessons/db/cache/lessons";
+import { wherePublicLessons } from "@/features/lessons/permissions/lessons";
+import { getProductIdTag } from "@/features/products/db/cache";
+import { wherePublicProducts } from "@/features/products/permissions/products";
+import { formatPrice } from "@/lib/formatters";
+import { sumArray } from "@/lib/sumArray";
+import { getUserCoupon } from "@/lib/userCountryHeader";
+import { and, asc, eq } from "drizzle-orm";
+import { cacheTag } from "next/dist/server/use-cache/cache-tag";
+import { notFound } from "next/navigation";
+import { Suspense } from "react";
+
+export default async function ProductPage({
+  params,
+}: {
+  params: Promise<{ productId: string }>;
+}) {
+  const { productId } = await params;
+  const product = await getPublicProduct(productId);
+
+  if (product == null) return notFound();
+  const courseCount = product.courses.length;
+  const lessonCount = sumArray(product.courses, (course) =>
+    sumArray(course.courseSections, (s) => s.lessons.length)
+  );
+
+  return (
+    <div className="container my-6">
+      <div className="flex gap-16 items-center justify-between">
+        <div className="flex gap-6 flex-col items-start">
+          <div className="flex flex-col gap-2">
+            <Suspense
+              fallback={
+                <div className="text-xl">
+                  {formatPrice(product.priceInDollars)}
+                </div>
+              }
+            >
+              <Price price={product.priceInDollars} />
+            </Suspense>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+async function Price({ price }: { price: number }) {
+  const coupon = await getUserCoupon();
+
+  if (price === 0 || coupon == null) {
+    return <div className="text-xl">{formatPrice(price)}</div>;
+  }
+
+  return (
+    <div className="flex gap-2 items-baseline">
+      <div className="line-through text-sm opacity-50">
+        {formatPrice(price)}
+      </div>
+      <div className="text-xl">
+        {formatPrice(price * (1 - coupon.discountPercentage))}
+      </div>
+    </div>
+  );
+}
+
+async function getPublicProduct(id: string) {
+  "use cache";
+  cacheTag(getProductIdTag(id));
+
+  const product = await db.query.ProductTable.findFirst({
+    columns: {
+      id: true,
+      name: true,
+      description: true,
+      priceInDollars: true,
+      imageUrl: true,
+    },
+    where: and(eq(ProductTable.id, id), wherePublicProducts),
+    with: {
+      courseProducts: {
+        columns: {},
+        with: {
+          course: {
+            columns: { id: true, name: true },
+            with: {
+              courseSections: {
+                columns: { id: true, name: true },
+                where: wherePublicCourseSections,
+                orderBy: asc(CourseSectionTable.order),
+                with: {
+                  lessons: {
+                    columns: { id: true, name: true, status: true },
+                    where: wherePublicLessons,
+                    orderBy: asc(LessonTable.order),
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+
+  if (product == null) return product;
+
+  cacheTag(
+    ...product.courseProducts.flatMap((cp) => [
+      getLessonCourseTag(cp.course.id),
+      getCourseSectionCourseTag(cp.course.id),
+      getCourseIdTag(cp.course.id),
+    ])
+  );
+
+  const { courseProducts, ...other } = product;
+
+  return {
+    ...other,
+    courses: courseProducts.map((cp) => cp.course),
+  };
+}
