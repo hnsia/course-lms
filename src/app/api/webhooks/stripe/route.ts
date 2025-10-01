b573d26cf7c876e0427db90543ca1e7156e925cd
hnsia <44498178+hnsia@users.noreply.github.com>
2025-10-01 20:07:11 +0800

    feat: setup stripe webhook API

---
+++ src/app/api/webhooks/stripe/route.ts
@@ -0,0 +1,72 @@
+import { db } from "@/drizzle/db";
+import { ProductTable, UserTable } from "@/drizzle/schema";
+import { stripeServerClient } from "@/services/stripe/stripeServer";
+import { eq } from "drizzle-orm";
+import { redirect } from "next/navigation";
+import { NextRequest } from "next/server";
+import Stripe from "stripe";
+
+export async function GET(request: NextRequest) {
+    const stripeSessionId = request.nextUrl.searchParams.get("stripeSessionId")
+    if (stripeSessionId == null) redirect("/products/purchase-failure")
+
+    let redirectUrl: string
+    try {
+        const checkoutSession = await stripeServerClient.checkout.sessions.retrieve(stripeSessionId, {expand: ["line_items"]})
+        const productId = await processStripeCheckout(checkoutSession)
+    }
+}
+
+export async function POST() {
+
+}
+
+async function processStripeCheckout(checkoutSession:Stripe.Checkout.Session) {
+    const userId = checkoutSession.metadata?.userId
+    const productId = checkoutSession.metadata?.productId
+
+    if (userId == null || productId == null) {
+        throw new Error("Missing metadata")
+    }
+
+    const [product, user] = await Promise.all([
+        getProduct(productId),
+        await getUser(userId)
+    ])
+
+    if (product == null) throw new Error("Product not found")
+    if (user == null) throw new Error("User not found")
+
+    const courseIds = product.courseProducts.map(cp => cp.courseId)
+    addUserCourseAccess({ userId: user.id, courseIds})
+    insertPurchase({
+        stripeSessionId: checkoutSession.id, 
+        pricePaidInCents: checkoutSession.amount_total || product.priceInDollars * 100,
+        productDetails: product,
+        userId: user.id,
+        productId
+    })
+}
+
+function getProduct(id: string) {
+    return await db.query.ProductTable.findFirst({
+        columns: {
+            id: true,
+            priceInDollars: true,
+            name: true,
+            description: true,
+            imageUrl: true
+        },
+        where: eq(ProductTable.id, id),
+        with: {
+            courseProducts: { columns: { courseId: true} }
+        }
+    })
+}
+
+async function getUser(id: string) {
+    return await db.query.UserTable.findFirst({
+        columns: { id: true },
+        where: eq(UserTable.id, id)
+    })
+}
