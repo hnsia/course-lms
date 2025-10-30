d2f6622ec90a99bd4d766903b317a14c545361af
hnsia <44498178+hnsia@users.noreply.github.com>
2025-10-30 17:46:50 +0800

    feat: update course lesson sidebar accordion

--- src/app/(consumer)/courses/[courseId]/_client.tsx
+++ src/app/(consumer)/courses/[courseId]/_client.tsx
@@ -2,9 +2,14 @@
 
 import {
   Accordion,
+  AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { CheckCircle2Icon, VideoIcon } from "lucide-react";
+import Link from "next/link";
 import { useParams } from "next/navigation";
 
 export function CoursePageClient({
@@ -32,12 +37,37 @@ export function CoursePageClient({
       : course.courseSections[0];
 
   return (
-    <Accordion type="multiple">
+    <Accordion
+      type="multiple"
+      defaultValue={defaultValue ? [defaultValue.id] : undefined}
+    >
       {course.courseSections.map((section) => (
         <AccordionItem key={section.id} value={section.id}>
           <AccordionTrigger className="text-lg">
             {section.name}
           </AccordionTrigger>
+          <AccordionContent className="flex flex-col gap-1">
+            {section.lessons.map((lesson) => (
+              <Button
+                variant="ghost"
+                asChild
+                key={lesson.id}
+                className={cn(
+                  "justify-start",
+                  lesson.id === lessonId &&
+                    "bg-accent/75 text-accent-foreground"
+                )}
+              >
+                <Link href={`/courses/${course.id}/lessons/${lesson.id}`}>
+                  <VideoIcon />
+                  {lesson.name}
+                  {lesson.isComplete && (
+                    <CheckCircle2Icon className="ml-auto" />
+                  )}
+                </Link>
+              </Button>
+            ))}
+          </AccordionContent>
         </AccordionItem>
       ))}
     </Accordion>
