7351b22667a3b6cad548c84263347c712637d667
hnsia <44498178+hnsia@users.noreply.github.com>
2025-11-10 18:40:41 +0800

    feat: setup admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,91 @@
-export default function AdminPage() {
-  return <h1>TODO: Fix</h1>;
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { db } from "@/drizzle/db";
+import { PurchaseTable } from "@/drizzle/schema";
+import { getPurchaseGlobalTag } from "@/features/purchases/db/cache";
+import { formatNumber, formatPrice } from "@/lib/formatters";
+import { count, countDistinct, isNotNull, sql, sum } from "drizzle-orm";
+import { cacheTag } from "next/dist/server/use-cache/cache-tag";
+import { ReactNode } from "react";
+
+export default async function AdminPage() {
+  const {
+    averageNetPurchasesPerCustomer,
+    netPurchases,
+    netSales,
+    refundedPurchases,
+    totalRefunds,
+  } = await getPurchaseDetails();
+
+  return (
+    <div className="container my-6">
+      <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 md:grid-cols-4 gap-4">
+        <StatCard title="Net Sales">{formatPrice(netSales)}</StatCard>
+        <StatCard title="Refunded Sales">{formatPrice(totalRefunds)}</StatCard>
+        <StatCard title="Un-Refunded Purchases">
+          {formatNumber(netPurchases)}
+        </StatCard>
+        <StatCard title="Refunded Purchases">
+          {formatNumber(refundedPurchases)}
+        </StatCard>
+        <StatCard title="Purchases Per User">
+          {formatNumber(averageNetPurchasesPerCustomer, {
+            maximumFractionDigits: 2,
+          })}
+        </StatCard>
+      </div>
+    </div>
+  );
+}
+
+function StatCard({ title, children }: { title: string; children: ReactNode }) {
+  return (
+    <Card>
+      <CardHeader className="text-center">
+        <CardDescription>{title}</CardDescription>
+        <CardTitle className="font-bold text-2xl">{children}</CardTitle>
+      </CardHeader>
+    </Card>
+  );
+}
+
+async function getPurchaseDetails() {
+  "use cache";
+  cacheTag(getPurchaseGlobalTag());
+
+  const data = await db
+    .select({
+      totalSales: sql<number>`COALESCE(${sum(
+        PurchaseTable.pricePaidInCents
+      )}, 0)`.mapWith(Number),
+      totalPurchases: count(PurchaseTable.id),
+      totalUsers: countDistinct(PurchaseTable.userId),
+      isRefund: isNotNull(PurchaseTable.refundedAt),
+    })
+    .from(PurchaseTable)
+    .groupBy((table) => table.isRefund);
+
+  const [refundData] = data.filter((row) => row.isRefund);
+  const [salesData] = data.filter((row) => !row.isRefund);
+
+  const netSales = (salesData?.totalSales ?? 0) / 100;
+  const totalRefunds = (refundData?.totalSales ?? 0) / 100;
+  const netPurchases = salesData?.totalPurchases ?? 0;
+  const refundedPurchases = refundData?.totalPurchases ?? 0;
+  const averageNetPurchasesPerCustomer =
+    salesData?.totalUsers != null && salesData.totalUsers > 0
+      ? netPurchases / salesData.totalUsers
+      : 0;
+
+  return {
+    netSales,
+    totalRefunds,
+    netPurchases,
+    refundedPurchases,
+    averageNetPurchasesPerCustomer,
+  };
 }
